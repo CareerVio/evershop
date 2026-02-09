f6f1237e5e6d677caafdef5797f76edae81954bc
peneik <info.naifaro@gmail.com>
2026-02-09 09:05:26 +0700

    initialize evershop-cli

--- packages/evershop/src/modules/oms/services/index.ts
+++
@@ -1,6 +0,0 @@
-import addOrderActivityLog from './addOrderActivityLog.js';
-import cancelOrder from './cancelOrder.js';
-import createShipment from './createShipment.js';
-export * from './updatePaymentStatus.js';
-export * from './updateShipmentStatus.js';
-export { cancelOrder, addOrderActivityLog, createShipment };
